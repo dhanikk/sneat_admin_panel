9095a90204fb8c05be9f6d463628ea8eed29cd5f
archanau@itpathsolutions.com <archanau@itpathsolutions.com>
2024-11-14 10:22:53 +0530

    Dashboard layout change

--- routes/web.php
+++ routes/web.php
@@ -5,6 +5,7 @@ use App\Http\Controllers\LanguageController;
 use App\Http\Controllers\Auth\LoginController;
 use App\Http\Controllers\User\DashboardController;
 use App\Http\Controllers\RoleController;
+use App\Http\Controllers\UserController;
 use App\Http\Controllers\PermissionController;
 
 Route::get('/', function () {
@@ -19,26 +20,40 @@ Route::get('/logout', [LoginController::class, 'logout'])->name('logout');
 
 Route::post('/login', [LoginController::class, 'login'])->name('login');
 Route::group(['middleware' => ['auth']], function(){
+
     Route::get("dashboard", [DashboardController::class, 'index'])->name('dashboard');
+
     Route::controller(PermissionController::class)->group(function () {
-        Route::get('/permission', 'index')->name('permission.index')->middleware('permission:view.permission');
-        Route::get('permission/create', 'create')->name('permission.create')->middleware('permission:create.permission');
+        Route::get('/permission', 'index')->name('permission.index');
+        Route::get('permission/create', 'create')->name('permission.create');
         Route::post('permission/store', 'store')->name('permission.store');
-        Route::get('permission/{permission}/edit', 'edit')->name('permission.edit')->middleware('permission:update.permission');
+        Route::get('permission/{permission}/edit', 'edit')->name('permission.edit');
         Route::post('permission/update', 'update')->name('permission.update');
-        Route::delete('permission/{permission}', 'destroy')->name('permission.destroy')->middleware('permission:delete.permission');
+        Route::delete('permission/{permission}', 'destroy')->name('permission.destroy');
         Route::post('permission/moduleStore', 'moduleStore')->name('permission.module');
         Route::post('permission/delete', 'deleteSinglePermission')->name('permission.delete');
     });
+
     Route::controller(RoleController::class)->group(function () {
-        Route::get('/role', 'index')->name('role.index')->middleware('permission:view.role');
-        Route::get('role/create', 'create')->name('role.create')->middleware('permission:create.role');
+        Route::get('/role', 'index')->name('role.index');
+        Route::get('role/create', 'create')->name('role.create');
         Route::post('role/store', 'store')->name('role.store');
-        Route::get('role/{role}/edit', 'edit')->name('role.edit')->middleware('permission:update.role');
+        Route::get('role/{role}/edit', 'edit')->name('role.edit');
         Route::post('role/{role}', 'update')->name('role.update');
-        Route::delete('role/{role}', 'destroy')->name('role.destroy')->middleware('permission:delete.role');
+        Route::delete('role/{role}', 'destroy')->name('role.destroy');
         Route::post('/role/{roleId}/updateStatus', 'updateStatus')->name('role.updateStatus');
     });
+
+    Route::controller(UserController::class)->group(function () {
+        Route::get('/user', 'index')->name('user.index');
+        Route::get('user/create', 'create')->name('user.create');
+        Route::post('user/store', 'store')->name('user.store');
+        Route::get('user/{user}/edit', 'edit')->name('user.edit');
+        Route::post('user/{user}', 'update')->name('user.update');
+        Route::delete('user/{user}', 'destroy')->name('user.destroy');
+        Route::post('/user/{userId}/updateStatus', 'updateStatus')->name('user.updateStatus');
+    });
+
 });
 
 
